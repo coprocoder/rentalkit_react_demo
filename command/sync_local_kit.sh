a176fd609e24ebedcd0e97fd47cf45a7a0891299
coprocoder <elijah.krikunov@gmail.com>
2024-02-21 02:57:16 +0700

    script sync local

--- command/sync_local_kit.sh
+++ command/sync_local_kit.sh
@@ -2,14 +2,14 @@
 set -x #echo on
 
 readonly ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-readonly INVEST_LOCAL_DIR="/home/coprocoder/Projects/rentalkit"
+readonly LOCAL_DIR="/home/coprocoder/Projects/rentalkit"
 
 declare -a arr=(
   "core"
 )
 
 for i in "${arr[@]}"; do
-  source="${INVEST_LOCAL_DIR}/${i}"
+  source="${LOCAL_DIR}/${i}"
   target="${ROOT_DIR}/node_modules/@rentalkit/${i}"
   [ -d "$target" ] && rm -r "$target"
   cp -r "$source" "$target"
